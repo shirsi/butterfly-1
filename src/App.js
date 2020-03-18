cfac9def9d47433382643641a7ef1bb052a3a4e4
Matthew Kangvanvongsa <matthewmetaveemomo@Matthews-MacBook-Pro.local>
2020-03-18 12:22:41 -0500

    push changes

--- src/App.js
+++ src/App.js
@@ -38,13 +38,19 @@ class App extends React.Component {
     this.state = {
       posts:[],
       username:'',
-      post: null
+      comments:''
+      post: null,
+      signup: false
     }
     this.handleAddPost = this.handleAddPost.bind(this)
     this.deletePost = this.deletePost.bind(this)
     this.getPosts = this.getPosts.bind(this)
     this.handleUpdatePost = this.handleUpdatePost.bind(this)
+    this.getSession = this.getSession.bind(this)
     this.handleSignin = this.handleSignin.bind(this)
+    this.toggleLikes = this.toggleLikes.bind(this)
+    this.handleUpdateComments = this.handleUpdateComments.bind(this)
+    this.logoutUser = this.logoutUser.bind(this)
     // this.getCurrentUser = this.getCurrentUser.bind(this)
   }
 
@@ -58,16 +64,43 @@ class App extends React.Component {
 
     componentDidMount(){
       this.getPosts()
+      this.getSession()
       // this.getCurrentUser()
     }
-
-
 
     /*
      ********************************************************
                 GRABS POST FROM SERVER
      ********************************************************
      */
+
+
+
+    async getSession(){
+      try {
+        let response = await fetch(`${baseURL}/sessions`)
+
+        let data = await response.json()
+        console.log(data)
+
+        this.setState({
+          session: data
+        })
+      } catch(e){
+        console.error(e);
+      }
+    }
+
+
+
+
+
+   /*
+   ********************************************************
+              GRABS POST FROM SERVER
+   ********************************************************
+   */
+
         async getPosts(){
           try {
             let response = await fetch(`${baseURL}/butterfly`)
@@ -85,6 +118,7 @@ class App extends React.Component {
 
         getPost(post){
           this.setState({post: post})
+          console.log(this.state.post);
         }
 
      /*
@@ -101,7 +135,7 @@ class App extends React.Component {
             title:  '',
             media: '',
             caption: '',
-
+            comments:''
           })
         }
 
@@ -118,7 +152,36 @@ class App extends React.Component {
                  Comments
        ********************************************************
        */
+       async handleUpdateComments(event, post, comment){
+         event.preventDefault()
+         // console.log(post._id);
+         let copyComments = [...this.state.post.comments]
+         // console.log(copyComments);
+         copyComments.push(comment)
+         try{
+           let response = await fetch(`${baseURL}/butterfly/${post._id}`,{
+             method:'PUT',
+             body: JSON.stringify({
+               comments: copyComments
+             }),
+             headers:{
+               'Content-Type': 'application/json'
+             }
+           })
+           let updatedPost = await response.json()
 
+           const foundPostIndex = this.state.posts.findIndex(foundPost => foundPost._id === post._id)
+           const copyPosts = [...this.state.posts]
+           copyPosts[foundPostIndex].comments = updatedPost.comments
+           console.log(copyPosts[foundPostIndex]);
+           this.setState({
+             posts: copyPosts,
+             // post:updatedPost
+           })
+         }catch(error){
+           console.error(error);
+         }
+       }
        /*
      ********************************************************
               update POSTS
@@ -140,13 +203,57 @@ class App extends React.Component {
          const copyPosts = [...this.state.posts]
          copyPosts[foundPostIndex] = updatedPost
          this.setState({
-           posts: copyPosts
+           posts: copyPosts,
+
          })
-       }catch(e){
-         console.error(e);
+       }catch(error){
+         console.error(error);
        }
      }
 
+
+
+
+     /*
+    ********************************************************
+            Update likes POSTS
+    ********************************************************
+    */
+
+    async toggleLikes (post){
+         // console.log(post)
+         try {
+           let response = await fetch( `${baseURL}/butterfly/${post._id}`, {
+             method: 'PUT',
+             body: JSON.stringify({likes: post.likes + 1}),
+             headers:{
+               'Content-type': 'application/json'
+             }
+           })
+
+
+           let updatedPost =  await response.json()
+
+           // console.log(updatedPost)
+
+           const foundPost = this.state.posts.findIndex(postFound=>
+             postFound._id === post._id
+           )
+           // console.log(foundPost);
+
+         const copyPosts = [...this.state.posts]
+         // console.log(copyPosts);
+         copyPosts[foundPost].likes = updatedPost.likes
+
+         // console.log(updatedPost);
+         this.setState({
+           posts: copyPosts
+         })
+
+       }catch(e){
+         console.error(e)
+       }
+       }
        /*
      ********************************************************
                Delete POSTS
@@ -160,23 +267,36 @@ class App extends React.Component {
          let response = await fetch(`${baseURL}/butterfly/${id}`, {
            method: 'DELETE'
          })
-
          let data = await response.json()
-
          const deletedPost = this.state.posts.findIndex(post =>
          post._id === id)
-
          const copyPosts = [...this.state.posts]
-
          copyPosts.splice(deletedPost, 1)
-
          this.setState({
            posts: copyPosts
          })
-
-
-
        } catch(e){
+         console.error(e);
+       }
+     }
+     /*
+   ********************************************************
+             Delete USERS
+   ********************************************************
+   */
+     async logoutUser(){
+      try{
+         let response = await fetch(`${baseURL}/sessions`,{
+           method:'DELETE'
+         })
+         let data = await response
+         console.log(data)
+         this.setState({
+           username:'',
+           password:'',
+           signup:false
+         })
+       }catch(e){
          console.error(e);
        }
      }
@@ -184,33 +304,51 @@ class App extends React.Component {
   render(){
   return (
     <div className="App">
+
     <Signup baseURL={baseURL}/>
     {
       this.state.username
       ?
+      <div>
       <h1>Hi, {this.state.username}</h1>
+      <button onClick = {() => {
+        this.logoutUser()
+      }}>Logout</button>
+      </div>
       :
       <Signin
       handleSignin = {this.handleSignin}
       baseURL={baseURL}
       username={this.state.username}/>
     }
-
+      <button onClick={
+        this.signOutUser
+      }>Sign Out</button>
       <New baseURL={baseURL} handleAddPost={this.handleAddPost}/>
 
         {
             this.state.posts.map(post =>{
               return(
                 <div>
-                  <h2 onClick={()=>this.getPost(post)}>{post.title}</h2>
+                  <h2 onClick={()=>this.getPost(post)}>{post.title.toUpperCase()}</h2>
                   <button onClick={() => {
                     this.deletePost(post._id)
                   }}>delete</button>
+
+
                   <Post post={post} handleUpdatePost={this.handleUpdatePost}/>
+                <div onClick={() => {
+                    this.toggleLikes(post)
+                  }}>{
+                    post.likes? '❤️': '🤍'
+                  }</div>
                 </div>
               )}
             )}
-            {this.state.post ? <Show post={this.state.post}/> : null}
+            {this.state.post
+              ? <Show
+              post={this.state.post} handleUpdateComments={this.handleUpdateComments}/>
+              : null}
     </div>
   )}
 }
